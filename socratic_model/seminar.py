60db33c3229d41ebcc531125716da727d6aab37b
ryanchaiyakul <ryanchaiyakul@gmail.com>
2024-10-19 17:09:52 -0700

    added to_cha functionality
    Verified with CLAN program

--- socratic_model/seminar.py
+++ socratic_model/seminar.py
@@ -1,5 +1,7 @@
 import typing
 import lzma
+import datetime
+import re
 
 from . import actor
 
@@ -10,7 +12,7 @@ _example_prompt = "{} \
 If a part starts with the characters \"[PROMPT]\", \
 this message comes from the user and not your opponent. \
 You may not use the characters \"[PROMPT]\" in your response. \
-If you recieve one of these messages after your most recent output, \
+If you receive one of these messages after your most recent output, \
 please acknowledge and respond to it. After the next sentence, all \
 responses from the user will be from your opponent. Can you begin the debate?"
 
@@ -29,24 +31,26 @@ class Seminar:
     def to_dict(self, model_encoding: dict = DEFAULT_ENCODING) -> dict:
         content_encoded = []
         for k, v in self.__contents:
-            content_encoded.append(lzma.compress("{}\n{}".format(k,v).encode('utf-8')))
-        return {"content" : content_encoded, "models": [model_encoding[type(model)] for model in self.__models], "index": self.__index}
+            content_encoded.append(lzma.compress(
+                "{}\n{}".format(k, v).encode('utf-8')))
+        return {"content": content_encoded, "models": [model_encoding[type(model)] for model in self.__models], "index": self.__index}
 
-    def last_content_embedded(self)->dict:
-        return {"index": self.__index, "content" : firestore.ArrayUnion([lzma.compress("{}\n{}".format(self.__contents[-1][0],self.__contents[-1][1]).encode('utf-8'))])}
+    def last_content_embedded(self) -> dict:
+        return {"index": self.__index, "content": firestore.ArrayUnion([lzma.compress("{}\n{}".format(self.__contents[-1][0], self.__contents[-1][1]).encode('utf-8'))])}
 
     @staticmethod
     def from_dict(config: dict, model_encoding: dict = DEFAULT_ENCODING) -> "Seminar":
         inv_encoding = {v: k for k, v in model_encoding.items()}
-        seminar = Seminar("NOT IMPORTANT", *[inv_encoding[encoding]() for encoding in config['models']])
-        
+        seminar = Seminar(
+            "NOT IMPORTANT", *[inv_encoding[encoding]() for encoding in config['models']])
+
         content = []
         for encoded in config['content']:
-            content.append(lzma.decompress(encoded).decode('utf-8').split('\n', 1))
+            content.append(lzma.decompress(
+                encoded).decode('utf-8').split('\n', 1))
         seminar._Seminar__contents = content
         seminar._Seminar__index = config['index']
         return seminar
-
 
     def __init_contents(self, prompt: str):
         self.__contents.append((0, _example_prompt.format(prompt)))
@@ -71,6 +75,25 @@ class Seminar:
                                        0 else "Participant {}".format(num), content)
         return ret
 
+    def to_cha(self):
+        """
+        Return the seminar as a cha file
+        """
+        llms = "".join(["SPE{} LLM{} Speaker, ".format(i, i)
+                       for i in range(len(self.__models))])
+        IDs = "".join(["@ID:\teng|llm_debate|HST|||||Host|||\n"] +
+                      ["@ID:\teng|llm_debate|SPE{}|||||Speaker|||\n".format(i) for i in range(len(self.__models))])
+        ret = "@UTF8\n@Begin\n@Languages:\teng\n@Participants:\tHST Prompt Host, {}\n{}@Date:\t{}\n".format(
+            llms[:-2], IDs, datetime.datetime.today().strftime('%d-%b-%Y').upper())
+
+        for num, content in self.contents:
+            # Split into sentences
+            for line in [s.strip() for s in re.findall(r'[^.!?]+[.!?]', content)]:
+                ret += "*{}:\t{}\n".format("HST" if num == 0 else "SPE{}".format(
+                    num-1), line.replace("\"[PROMPT]\"", "&~PROMPT"))
+
+        return ret + "@End"
+
     def talk(self, index: int):
         if index < 0 or index > len(self.__models):
             raise ValueError("Index: {} is out of range".format(index))
@@ -79,7 +102,7 @@ class Seminar:
             self.contents, index+1)
         self.__contents.append(
             (index+1, new_content))
-        
+
     def next(self):
         """
         Calls talk for the next NPC participant
